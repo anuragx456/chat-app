ef10535e4a7f6c717ac5c0a0ed289ee7ec8cc906
Anuragx456 <anuragcolab0@gmail.com>
2026-03-29 14:31:14 +0530

    Completed UserCard component

--- chat-app/app/(tabs)/discover.tsx
+++ chat-app/app/(tabs)/discover.tsx
@@ -1,14 +1,83 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ActivityIndicator, FlatList, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+import { useDiscoverUsers, useSendFriendRequest } from '@/hooks/useFriendQueries';
+import { UserCard } from '@/components/UserCard';
 
 const DiscoverScreen = () => {
+    const [search, setSearch] = useState("");
+    const {data:users=[], isLoading} = useDiscoverUsers(search);
+    const sendRequestMutation = useSendFriendRequest();
+    // const acceptRequestMutation = useAcceptFriendRequest();
+
+    const handleSendRequest = async(receiverId: string) => {
+        sendRequestMutation.mutate(receiverId)
+    }
+
     return (
-    <View>
-        <Text>DiscoverScreen</Text>
+    <View style={styles.container}>
+        <Text style={styles.header}>Discover People</Text>
+        <TextInput 
+        style={styles.searchInput}
+        placeholder='Search by Email, Name...'
+        placeholderTextColor={"#aaa"}
+        value={search}
+        onChangeText={setSearch}
+        />
+
+        {
+            isLoading && !users.length ? (
+                <ActivityIndicator size={"large"} color={"#007aff"} style={{ marginTop: 20}} />
+            ) : (
+                <FlatList 
+                data={users}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) => (
+                    <UserCard 
+                    user={item}
+                    onSendRequest={handleSendRequest}
+                    onAcceptRequest={() => {}}
+                    onRejectRequest={() => {}}
+                    onCancelRequest={() => {}}
+                    />
+                )}
+                ListEmptyComponent={
+                    <Text style={styles.emptyText}>No Users Found.</Text>
+                }
+                contentContainerStyle={{paddingBottom: 20}}
+                />
+            )
+        }
     </View>
     )
 }
 
 export default DiscoverScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: "#121212",
+    },
+    header: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "#fff",
+        marginBottom: 16,
+        marginTop: 40,
+    },
+    searchInput: {
+        backgroundColor: "#1e1e1e",
+        color: "#fff",
+        padding: 12,
+        borderRadius: 8,
+        marginBottom: 16,
+        borderWidth: 1,
+        borderColor: "#333",
+    },
+    emptyText: {
+        color: "#aaa",
+        textAlign: "center",
+        marginTop: 20,
+    },
+})
